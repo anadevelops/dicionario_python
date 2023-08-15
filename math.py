294c0bf7b058932f49edfa531e7013e14b975ff4
anadevelops <ana.stupp02@gmail.com>
2023-08-15 12:37:21 -0300

    .

---
+++ math.py
@@ -0,0 +1,35 @@
+#Operações aritiméticas
+
+#4 operações básicas
+soma = 3 + 4
+print(soma)
+subtracao = 3 - 2
+print(subtracao)
+multiplicacao = 3 * 2
+print(multiplicacao)
+divisao = 10 / 2
+print(divisao)
+
+#Divisão inteira, sem resto
+divInteira = 3//4
+print(divInteira)
+
+#Resto de divisão inteira
+restoDiv = 3%4
+print(restoDiv)
+
+#Potenciação
+potenciacao = 3**4
+print(potenciacao)
+
+#--------------------------------------------
+#Arredontamento
+x = round(2.7)
+print(x)
+
+#--------------------------------------------
+#Notação científica
+print(1.2e10)
+print(3.4e-10)
+
+
